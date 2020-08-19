e11370524affa2ae1b5556ceddde17d63b114e84
Timo Seitz <tpseitz@mbnet.fi>
2020-08-19 21:04:45 +0300

    Removed SECRET_KEY from project.settings and set logic for local settings

--- django/project/settings.py
+++ django/project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_wv=^wzo#&)aj$rozljcxqjd*pi6k9e+2ive3fn=w(8$m*5$i9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
