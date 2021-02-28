a838d46be5aa789844f959c12cda6ffb551314e0
Timo Seitz <tpseitz@mbnet.fi>
2021-02-28 18:11:07 +0200

    Updated local_settings for docker

--- django/local_settings_docker.py
+++ django/local_settings_docker.py
@@ -1,8 +1,21 @@
 from project.settings import *
 
+from os import getenv
+
 DEBUG = True
 
 SECRET_KEY = 'REPLACE WITH REAL SECRET'
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': getenv('POSTGRES_DB'),
+    'USER': getenv('POSTGRES_USER'),
+    'PASSWORD': getenv('POSTGRES_PASSWORD'),
+    'HOST': getenv('POSTGRES_HOST'),
+    'PORT': getenv('POSTGRES_PORT', 5432),
+  }
+}
 
 MEDIA_ROOT = '/home/sid/upload'
 
