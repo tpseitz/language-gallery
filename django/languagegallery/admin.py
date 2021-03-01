423b6ee6de06ad3a12efa7f1556eee0a675493fb
Timo Seitz <tpseitz@mbnet.fi>
2021-03-01 22:04:23 +0200

    Closes #6: Added models to django admin

--- django/languagegallery/admin.py
+++ django/languagegallery/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from languagegallery import models
 
-# Register your models here.
+
+@admin.register(models.MimeType)
+class MimeTypeAdmin(admin.ModelAdmin):
+  list_display = ('mimetype', 'extension', 'identifier')
+  ordering = ('mimetype', )
+  search_fields = ('mimetype', 'extension')
+
+
+@admin.register(models.MediaTag)
+class MimeTypeAdmin(admin.ModelAdmin):
+  search_fields = ('name', )
+
+
+@admin.register(models.FileInfo)
+class FileInfoAdmin(admin.ModelAdmin):
+  list_display = ('basename', 'title', 'mimetype')
+  ordering = ('basename', 'title')
+  search_fields = ('basename', 'title', 'mimetype__mimetype', 'mimetype__extension')
+
