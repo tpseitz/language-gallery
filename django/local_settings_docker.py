21e3f6570d68fdd06e68ec9769b45fc993b3edc9
Timo Seitz <timo@seitz.fi>
2024-12-15 16:59:09 +0200

    Add pymemcached as default storage

--- django/local_settings_docker.py
+++ django/local_settings_docker.py
@@ -19,9 +19,15 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CACHES['files'] = {
-  'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-  'LOCATION': '/home/sid/cache',
+CACHES = {
+  'default': {
+    'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+    'LOCATION': 'memcached:11211',
+  },
+  'files': {
+    'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+    'LOCATION': '/home/sid/cache',
+  },
 }
 
 MEDIA_ROOT = '/home/sid/mediaroot'
